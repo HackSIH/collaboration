5f4d2d1dfe5bdecb0e3998196d4ac3f6582e6ae8
medhavi0407 <medhavisaxena0407@gmail.com>
2023-09-23 20:42:32 +0530

    changes

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './components/Home';
+import Search from './components/Search';
+import Footer from './components/Footer';
+
+export default function App() {
+  return( 
+    <section>
+      
+      <Home/>
+      
+      <Search/>
+      <Footer/>
+     
+    </section>
+  )
+}
+

3ce881e083b9a8d776e05e84d669e114f51cb3dc
medhavi0407 <medhavisaxena0407@gmail.com>
2023-09-24 12:59:27 +0530

    feedback

---
+++ feedback form/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Form from "./Form";
+import Home from './Components/Home';
+import Footer from './Components/Footer';
+
+export default function App() {
+  
+  return <>
+  <Home/>
+   <Form/>
+   <Footer/>
+     </>
+ 
+}
